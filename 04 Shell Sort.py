f56af35f28278139608ecbc140f58aa9e9e538ca
Siva Kottapalli <krishna1540@gmail.com>
2018-03-20 15:40:22 -0400

    Add files via upload

--- 04 Shell Sort.py
+++ 04 Shell Sort.py
@@ -11,17 +11,17 @@ def Ashellsort(A):
         for i in range(n-gap):
             if A[i] >= A[i+gap]:
                 A[i], A[i+gap] = A[i+gap], A[i]
-            j = i   
-            while j-gap >= 0:
-                if A[j-gap] >= A[j]:
-                    A[j], A[j-gap] = A[j-gap], A[j]
-                    j = j-gap
-                else:
-                    j = 0
+                j = i   
+                while j-gap >= 0:
+                    if A[j-gap] >= A[j]:
+                        A[j], A[j-gap] = A[j-gap], A[j]
+                        j = j-gap
+                    else:
+                        j = 0
         gap = int(np.floor(gap/2))
     print(A)
 
-A = [700, 2, 101, 1, 3, 4, 54, 0,  -3]
+A = [100, 2, 101, 1, 3, 4, 54, 0, -3]
 Ashellsort(A)
 
 #Function to sort input array to descending order
@@ -33,15 +33,15 @@ def Dshellsort(A):
         for i in range(n-gap):
             if A[i] <= A[i+gap]:
                 A[i], A[i+gap] = A[i+gap], A[i]
-            j = i   
-            while j-gap >= 0:
-                if A[j-gap] <= A[j]:
-                    A[j], A[j-gap] = A[j-gap], A[j]
-                    j = j-gap
-                else:
-                    j = 0
+                j = i   
+                while j-gap >= 0:
+                    if A[j-gap] <= A[j]:
+                        A[j], A[j-gap] = A[j-gap], A[j]
+                        j = j-gap
+                    else:
+                        j = 0
         gap = int(np.floor(gap/2))
     print(A)
 
-A = [700, 2, 101, 1, 3, 4, 54, 0,  -3]
+A = [100, 2, 101, 1, 3, 4, 54, 0, -3]
 Dshellsort(A)
